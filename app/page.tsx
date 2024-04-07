280f3b4373f74b809c2b23937c7550a9bf9b533b
yorkkong <yorkkong@tencent.com>
2024-04-07 17:19:43 +0800

    feat: add drawer

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,35 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { useRouter } from 'next/navigation';
-import Box from '@mui/material/Box';
+import Link from "next/link";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useRouter } from "next/navigation";
+import Box from "@mui/material/Box";
+import { toolList } from "./toolbox/toolList";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
+  
+
+  const buttons = toolList.map((item) => (
+    <Button
+      key={item.Id}
+      variant="contained"
+      onClick={() => router.push(item.Path)}
+    >
+      {item.Name}
+    </Button>
+  ));
+
   return (
-    <main className="flex min-h-screen flex-col  items-center justify-center space-y-10 bg-yellow-50">
-      <Typography variant="h2" gutterBottom className=' grow-1'>
-        York's Tool Box
-      </Typography>
-      <div className="flex-wrap justify-center space-x-5 space-y-5" >
-        <Button variant="contained" onClick={() => router.push("/toolbox/rename_folders")}>批量修改文件名</Button>
+    <main className="flex min-h-screen flex-col  items-center  justify-around space-y-10 bg-yellow-50">
+      <div>
+        <Typography variant="h2" gutterBottom className=" grow-1">
+          {"York's Tool Box"}
+        </Typography>
+      </div>
+      <div className="flex-wrap justify-center space-x-5">
+        {buttons}
       </div>
     </main>
   );
---
+++ app/toolbox/toolList.ts
@@ -0,0 +1,7 @@
+export const toolList = [
+  {
+    Id: "rename_folders",
+    Name: "批量修改文件名",
+    Path: "/toolbox/rename_folders",
+  },
+];
